59da3b7531ca0107977e302dd9a1be550834f3db
Centiox <56820688+Centiox@users.noreply.github.com>
2019-10-22 08:54:19 +0200

    Add files via upload

--- src/test/java/fr/lp/ic/contact/ContactServiceMockTest.java
+++ src/test/java/fr/lp/ic/contact/ContactServiceMockTest.java
@@ -2,17 +2,21 @@ package fr.lp.ic.contact;
 
 import fr.lp.ic.contact.dao.IContactDao;
 import fr.lp.ic.contact.exception.ContactException;
-import fr.lp.ic.contact.exception.ContactNotFoundException;
 import fr.lp.ic.contact.model.Contact;
-import org.easymock.*;
+import org.easymock.EasyMock;
+import org.easymock.EasyMockRule;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-public class ContactServiceMockTest extends BaseMockTest {
+public class ContactServiceMockTest {
+
+    @Rule
+    public EasyMockRule rule = new EasyMockRule(this);
 
     @TestSubject
     ContactService contactService = new ContactService();
@@ -22,109 +26,22 @@ public class ContactServiceMockTest extends BaseMockTest {
 
     @Test(expected = ContactException.class)
     public void shouldFailIfNameAlreadyExists() throws ContactException {
-        String name = "Arnaud";
-        // Enregistrement des comportements
-        expect(contactDao.findByName(name))
-                .andReturn(Optional.of(new Contact()));
+        String name = "nicolas";
+        EasyMock.expect(contactDao.findByName(name)).andReturn(Optional.of(new Contact()));
+        EasyMock.replay(contactDao);
+        contactService.deleteContact(name);
 
-        // Fin de l'enregistrement
-        replayAll();
-
-        contactService.newContact(name, "0254414512", "test@yopmail.com");
-    }
-
-    @Test
-    public void shouldInsertElementExists() throws ContactException {
-        String name = "Arnaud";
-        // Enregistrement des comportements
-        expect(contactDao.findByName(name))
-                .andReturn(Optional.empty());
-        Capture<Contact> capturedContact = newCapture();
-
-        expect(contactDao.save(capture(capturedContact)))
-                .andReturn(true);
-
-        // Fin de l'enregistrement
-        replayAll();
-
-        contactService.newContact(name, "0254414512", "test@yopmail.com");
+        contactService.newContact(name, "0675852417", "test@yopmail.com");
         Contact value = capturedContact.getValue();
         Assert.assertEquals(name, value.getName());
-        Assert.assertEquals("Phone error", "0254414512", value.getPhone());
-        Assert.assertEquals("email error", "test@yopmail.com", value.getEmail());
+        Assert.assertEquals("Phone error", "0675852417", value.getPhone());
+        Assert.assertEquals("email error","test@yopmail.com", value.getEmail());
     }
 
-    @Test(expected = ContactNotFoundException.class)
-    public void shouldFailDeletionIfNameDoesntExist() throws ContactNotFoundException {
-        String name = "Arnaud";
-        //Record
-        EasyMock.expect(contactDao.findByName(name))
-                .andReturn(Optional.empty());
-        //End Record
+    @Test(expected = ContactException.class)
+    public void shouldShowSomeEasyMockSamples() throws ContactException {
+        String name = "nicolas";
+        EasyMock.expect(contactDao.findByName(name)).andReturn(Optional.of(new Contact()));
         EasyMock.replay(contactDao);
-        //Test
         contactService.deleteContact(name);
-        //Assertions
-    }
-
-    @Test
-    public void shouldDeleteIfNameExists() throws ContactNotFoundException {
-        String name = "Arnaud";
-        //Record
-        EasyMock.expect(contactDao.findByName(name))
-                .andReturn(Optional.of(new Contact()));
-        EasyMock.expect(contactDao.delete(name)).andReturn(true);
-        //End Record
-        EasyMock.replay(contactDao);
-        //Test
-        contactService.deleteContact(name);
-    }
-
-    @Test
-    public void shouldShowSomeEasyMockSamples() throws ContactNotFoundException {
-        String name = "Arnaud";
-        //Record
-        EasyMock.expect(contactDao.findByName(name)).andAnswer(new IAnswer<Optional<Contact>>() {
-            @Override
-            public Optional<Contact> answer() throws Throwable {
-                Thread.sleep(3000);
-                return Optional.empty();
-            }
-        });
-        //End Record
-        EasyMock.replay(contactDao);
-        //Test
-        contactService.deleteContact(name);
-    }
-
-    @Test
-    public void shouldListContact() throws ContactNotFoundException {
-
-        //Record
-        EasyMock.expect(contactDao.findAll()).andReturn(new ArrayList<>());
-        //End Record
-        EasyMock.replay(contactDao);
-        //Test
-        List<String> strings = contactService.listAll();
-        System.out.println(strings);
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void shouldCancelCallTimeout() {
-
-        //Record
-        EasyMock.expect(contactDao.findAll()).andAnswer(new IAnswer<List<Contact>>() {
-            @Override
-            public List<Contact> answer() throws Throwable {
-                Thread.sleep(3100);
-                return new ArrayList<>();
-            }
-        });
-        //End Record
-        EasyMock.replay(contactDao);
-        //Test
-        List<String> strings = contactService.listAll();
-
-    }
-
 }
